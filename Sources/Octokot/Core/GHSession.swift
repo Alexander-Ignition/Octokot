b2728947c8199a0de60c832884d65ef135eee50a
Alexander Ignition <izh.sever@gmail.com>
2022-01-15 12:42:57 +0300

    Update GHSession.swift

--- Sources/Octokot/Core/GHSession.swift
+++ Sources/Octokot/Core/GHSession.swift
@@ -36,25 +36,33 @@ extension URLSession {
         let session: URLSession
 
         func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+            #if os(Linux)
+            return try await _data(for: request)
+            #else
             if #available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *) {
                 return try await session.data(for: request, delegate: nil)
             } else {
-                return try await withCheckedThrowingContinuation { continuation in
-                    let task = session.dataTask(with: request) { data, response, error in
-                        switch (response, error) {
-                        case (_, let error?):
-                            continuation.resume(throwing: error)
-                        case (.none, _):
-                            let message = "Missing URLResponse"
-                            let error = URLError(.unknown, userInfo: [NSLocalizedDescriptionKey: message])
-                            continuation.resume(throwing: error)
-                        case (let response?, _):
-                            let body = data ?? Data()
-                            continuation.resume(returning: (body, response))
-                        }
+                return try await _data(for: request)
+            }
+            #endif
+        }
+
+        private func _data(for request: URLRequest) async throws -> (Data, URLResponse) {
+            try await withCheckedThrowingContinuation { continuation in
+                let task = session.dataTask(with: request) { data, response, error in
+                    switch (response, error) {
+                    case (_, let error?):
+                        continuation.resume(throwing: error)
+                    case (.none, _):
+                        let message = "Missing URLResponse"
+                        let error = URLError(.unknown, userInfo: [NSLocalizedDescriptionKey: message])
+                        continuation.resume(throwing: error)
+                    case (let response?, _):
+                        let body = data ?? Data()
+                        continuation.resume(returning: (body, response))
                     }
-                    task.resume()
                 }
+                task.resume()
             }
         }
     }
